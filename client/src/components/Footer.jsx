23d8ee8d35e3a03cdf7e5e296314755bb2861a20
Rahul Kumar <connectrahul25@gmail.com>
2023-08-14 15:18:42 +0530

    14-08-2023: Header and Footer UI Update (Rahul)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useFOFContext } from "../context/context";
 
 
-const Footer = ({websiteData}) => {
+const Footer = ({ websiteData }) => {
 
     const { currentPath } = useFOFContext();
     const validPaths = ['/', '/login', '/signup', '/allDetails', '/editAllDetails', 'searchFriends'];
@@ -15,54 +15,104 @@ const Footer = ({websiteData}) => {
         return (
 
             <footer>
-
                 <div className={style.div1}>
-                    <div className={style.stats_card}>
-                        <i className="fa-solid fa-user-large"></i>
-                        <h1>{websiteData.totalVisitors}+</h1>
-                        <p>Total Visitors</p>
-                    </div>
+                    <div className={style.stats_div}>
+                        <div>
+                            <i className="fa-solid fa-user-large"></i>
+                            <strong> {websiteData.totalVisitors}+</strong>
+                            <p>Total Website Visits</p>
+                        </div>
 
-                    <div className={style.stats_card}>
-                        <i className="fa-solid fa-users"></i>
-                        <h1>{websiteData.totalUsers}+</h1>
-                        <p>Registered Users</p>
-                    </div>
+                        <div>
+                            <i className="fa-solid fa-users"></i>
+                            <strong> {websiteData.totalUsers}+</strong>
+                            <p>Registered Users</p>
+                        </div>
 
-                    <div className={style.stats_card}>
-                        <i className="fa-solid fa-hand-holding-dollar"></i>
-                        <h1>{websiteData.donationAmount}+</h1>
-                        <p>Donation Amount($)</p>
+                        <div>
+                            <i className="fa-solid fa-hand-holding-dollar"></i>
+                            <strong> {websiteData.donationAmount}+</strong>
+                            <p>Donation Amount($)</p>
+                        </div>
                     </div>
                 </div>
 
-                <hr className={style.hrline} />
-
                 <div className={style.div2}>
-                    <div className={style.follow_contact_us}>
 
-                        <h1>Follow us</h1>
+                    <div className={style.policy_div}>
+                        <strong className={style.heading2}>Privacy Policy | Terms & Conditions </strong>
+                        <p className={style.para}>Copyright &#169; 2023 FOF - All Right Reserved  </p>
+                    </div>
 
-                        <div className={style.social_media}>
-                            <a href=""> <i className="fa-brands fa-instagram"> </i> </a>
-                            <a href=""> <i className="fa-brands fa-facebook"> </i> </a>
-                            <a href=""> <i className="fa-brands fa-twitter"> </i> </a>
-                            <a href=""> <i className="fa-brands fa-linkedin"> </i> </a>
-                            <a href=""> <i className="fa-solid fa-envelope"> </i> </a>
-                            <a href=""> <i className="fa-brands fa-youtube"> </i> </a>
+                    <div className={style.contact_div}>
+                        <strong className={style.heading2}>Contact Us</strong>
+                        <p className={style.para}>Email ID: fof25122@gmail.com</p>
+                        <p className={style.para}>Phone No: xxxxx-xxxxx</p>
+                    </div>
+
+                    <div className={style.follow_div}>
+                        <strong className={style.heading2}>Follow Us</strong>
+                        <div className={style.social_icons}>
+                            <i className="fab fa-facebook"></i>
+                            <i className="fab fa-instagram"></i>
+                            <i className="fab fa-twitter"></i>
+                            <i className="fab fa-youtube"></i>
                         </div>
                     </div>
 
-                    <div className={style.follow_contact_us}>
-
-                        <h1>Contact Us</h1>
-                        <p>Phone no: 91+ 9646565565</p>
-                        <p><a href="#">Email: fofwebsite@gmail.com</a></p>
-
-                    </div>
-
                 </div>
+
             </footer>
+
+            // <footer>
+
+            //     <div className={style.div1}>
+            //         <div className={style.stats_card}>
+            //             <i className="fa-solid fa-user-large"></i>
+            //             <h1>{websiteData.totalVisitors}+</h1>
+            //             <p>Total Visitors</p>
+            //         </div>
+
+            //         <div className={style.stats_card}>
+            //             <i className="fa-solid fa-users"></i>
+            //             <h1>{websiteData.totalUsers}+</h1>
+            //             <p>Registered Users</p>
+            //         </div>
+
+            //         <div className={style.stats_card}>
+            //             <i className="fa-solid fa-hand-holding-dollar"></i>
+            //             <h1>{websiteData.donationAmount}+</h1>
+            //             <p>Donation Amount($)</p>
+            //         </div>
+            //     </div>
+
+            //     <hr className={style.hrline} />
+
+            //     <div className={style.div2}>
+            //         <div className={style.follow_contact_us}>
+
+            //             <h1>Follow us</h1>
+
+            //             <div className={style.social_media}>
+            //                 <a href=""> <i className="fa-brands fa-instagram"> </i> </a>
+            //                 <a href=""> <i className="fa-brands fa-facebook"> </i> </a>
+            //                 <a href=""> <i className="fa-brands fa-twitter"> </i> </a>
+            //                 <a href=""> <i className="fa-brands fa-linkedin"> </i> </a>
+            //                 <a href=""> <i className="fa-solid fa-envelope"> </i> </a>
+            //                 <a href=""> <i className="fa-brands fa-youtube"> </i> </a>
+            //             </div>
+            //         </div>
+
+            //         <div className={style.follow_contact_us}>
+
+            //             <h1>Contact Us</h1>
+            //             <p>Phone no: 91+ 9646565565</p>
+            //             <p><a href="#">Email: fofwebsite@gmail.com</a></p>
+
+            //         </div>
+
+            //     </div>
+            // </footer>
         )
     }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,28 +5,66 @@ import style from './navbar.module.css';
 const Navbar = () => {
 
 	return (
+
+
 		<nav className={style.navbar}>
-			<label className={style.label}>FOF </label>
-
-			<input type="checkbox" className={style.click} />
-
-			<label className={`${style.label} ${style.menu}`}>
-				<i className="fa-solid fa-bars"></i>
-			</label>
-
+			<a href="#" className={style.navbar_logo}> FOF </a>
 			<ul>
-				<li> <Link to="/">        <i className="fa-solid fa-house-chimney"></i>          Home        </Link></li>
-				<li> <Link to="/searchFriends">  <i className="fa-solid fa-magnifying-glass"></i>Search      </Link></li>
-				<li> <Link to="/editAllDetails">    <i className="fa-solid fa-pen-to-square"></i>Edit Details</Link></li>
-				<li> <Link to="/about">   <i className="fa-solid fa-address-card"></i>           About       </Link></li>
-				<li> <Link to="#">        <i className="fa-solid fa-user-group"></i>                         </Link></li>
-				{/* <li> <Link to="#">        <i className="fa-solid fa-bell"></i>                           </Link></li> */}
-				<li> <Link to="#">         <i className="fa-solid fa-hand-holding-dollar"></i>                     </Link></li>
-				{/* <li> <Link to="/suggestions"> <i className="fa-solid fa-comments"></i>          Suggestions </Link></li> */}
-				<li> <Link to="/logout">      <i className="fa-solid fa-arrow-right-from-bracket"></i> Log Out  </Link></li>
-			</ul>
+				<li>
+					<Link className={style.selected} to="/"><i className="fas fa-home"></i></Link> 
+					<span className={style.navbar_link_name}>Home</span> 
+				</li>
 
+				<li>
+					<Link to="/searchFriends"><i className="fas fa-user"></i></Link> 
+					<span className={style.navbar_link_name}>Search Friend</span> 
+				</li>
+
+				<li>
+					<Link to="/editAllDetails"><i className="fas fa-chart-bar"></i></Link> 
+					<span className={style.navbar_link_name}>Edit Details</span> 
+				</li>
+
+				<li>
+					<Link to="/messages"><i className="fas fa-envelope"></i></Link> 
+					<span className={style.navbar_link_name}>Messages</span>
+				</li>
+
+				<li>
+					<Link to="/donate"><i className="fas fa-donate"></i></Link> 
+					<span className={style.navbar_link_name}>Donate</span>
+				</li>
+
+				<li>
+					<Link to="/logout"><i className="fas fa-sign-out-alt"></i></Link> 
+					<span className={style.navbar_link_name}>Logout</span>
+				</li>
+
+			</ul>
 		</nav>
+
+		// <nav className={style.navbar}>
+		// 	<label className={style.label}>FOF </label>
+
+		// 	<input type="checkbox" className={style.click} />
+
+		// 	<label className={`${style.label} ${style.menu}`}>
+		// 		<i className="fa-solid fa-bars"></i>
+		// 	</label>
+
+		// 	<ul>
+		// 		<li> <Link to="/">        <i className="fa-solid fa-house-chimney"></i>          Home        </Link></li>
+		// 		<li> <Link to="/searchFriends">  <i className="fa-solid fa-magnifying-glass"></i>Search      </Link></li>
+		// 		<li> <Link to="/editAllDetails">    <i className="fa-solid fa-pen-to-square"></i>Edit Details</Link></li>
+		// 		<li> <Link to="/about">   <i className="fa-solid fa-address-card"></i>           About       </Link></li>
+		// 		<li> <Link to="#">        <i className="fa-solid fa-user-group"></i>                         </Link></li>
+		// 		{/* <li> <Link to="#">        <i className="fa-solid fa-bell"></i>                           </Link></li> */}
+		// 		<li> <Link to="#">         <i className="fa-solid fa-hand-holding-dollar"></i>                     </Link></li>
+		// 		{/* <li> <Link to="/suggestions"> <i className="fa-solid fa-comments"></i>          Suggestions </Link></li> */}
+		// 		<li> <Link to="/logout">      <i className="fa-solid fa-arrow-right-from-bracket"></i> Log Out  </Link></li>
+		// 	</ul>
+
+		// </nav>
 	)
 
 }
